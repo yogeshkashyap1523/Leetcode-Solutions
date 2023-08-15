a23ae384e7b784d103b0341f4eb1da0aa1a628a7
yogeshkashyap1523 <126580273+yogeshkashyap1523@users.noreply.github.com>
2023-08-15 17:36:47 +0530

    Time: 4 ms (32.86%) | Memory: 6.1 MB (14.45%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double sol=1;
+        if(sol > 0)
+        {
+            sol = pow(x,n);
+        }
+        else
+        {
+            sol =pow(x,n);
+        } 
+        return sol;
+    }
+};
