13a62d10678180cfd1c9ad78a9e91e195cac3cf8
yogeshkashyap1523 <126580273+yogeshkashyap1523@users.noreply.github.com>
2023-08-19 19:19:40 +0530

    Time: 2 ms (41.29%) | Memory: 6 MB (27.33%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.cpp
+++ 2630-alternating-digit-sum/alternating-digit-sum.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int alternateDigitSum(int n) {
-        int ans = 0;
         vector<int> temp;
         while (n != 0) {
             temp.push_back(n%10);
             n /= 10;
         }
-        int e = 0, o = 0;
-        for (int i=temp.size()-1; i>=0; i--) {
-            if (i%2 == 0) {
-                e += temp[i];
-            }
-
-            else {
-                o += temp[i];
-            }
+        int eSum = 0, oSum = 0;
+        for (int i=temp.size()-1; i>=0; i--) 
+        {
+            if (i%2 == 0) 
+                eSum += temp[i];
+            else 
+                oSum += temp[i];
         }
-
         if ((temp.size()-1)%2 == 0)
-            return e-o;
+            return eSum - oSum;
         else
-            return o-e;
+            return oSum-eSum;
     }
 };
