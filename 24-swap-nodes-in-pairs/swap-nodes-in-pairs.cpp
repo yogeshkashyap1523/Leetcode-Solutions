7a9d12de07cc570a5eccd33b5aa5dfd63f386349
yogeshkashyap1523 <126580273+yogeshkashyap1523@users.noreply.github.com>
2023-10-26 09:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (60.61%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) 
+            return head;         //If there are less than 2 nodes in the given nodes, then no need to do anything just return the list as it is.
+		
+        ListNode* dummyNode = new ListNode();
+        
+        ListNode* prev=dummyNode;
+        ListNode* curr=head;
+        
+        while(curr && curr->next){
+            prev->next = curr->next;
+            curr->next = prev->next->next;
+            prev->next->next = curr;
+            
+            prev = curr;
+            curr = curr->next;
+        }
+        
+        return dummyNode->next;
+    }
+};
